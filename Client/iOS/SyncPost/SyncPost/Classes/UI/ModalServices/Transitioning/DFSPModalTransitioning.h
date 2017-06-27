e481eb2ebea1221d030e4d9acb64d7c743027193
Dmitry Feld <dmitry.feld@icloud.com>
2017-06-26 22:08:53 -0700

    Adding renamed Classes

---
+++ Client/iOS/SyncPost/SyncPost/Classes/UI/ModalServices/Transitioning/DFSPModalTransitioning.h
@@ -0,0 +1,13 @@
+//
+//  DFSPModalTransitioning.h
+//  SyncPost
+//
+//  Created by Dmitry Feld on 6/16/17.
+//  Copyright © 2017 Dmitry Feld. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DFSPModalTransitioning : NSObject<UIViewControllerAnimatedTransitioning>
+@property (nonatomic, assign) BOOL isPresenting;
+@end
---
+++ Client/iOS/SyncPost/SyncPost/Classes/UI/ModalServices/Transitioning/DFSPModalTransitioning.m
@@ -0,0 +1,34 @@
+//
+//  DFSPModalTransitioning.m
+//  SyncPost
+//
+//  Created by Dmitry Feld on 6/16/17.
+//  Copyright © 2017 Dmitry Feld. All rights reserved.
+//
+
+#import "DFSPModalTransitioning.h"
+
+@implementation DFSPModalTransitioning
+//===================================================================
+// - UIViewControllerAnimatedTransitioning
+//===================================================================
+
+- (NSTimeInterval)transitionDuration:(id <UIViewControllerContextTransitioning>)transitionContext {
+    return 0.25f;
+}
+
+- (void)animateTransition:(id <UIViewControllerContextTransitioning>)transitionContext {
+    UIView *inView = [transitionContext containerView];
+    UIViewController *toVC = (UIViewController *)[transitionContext viewControllerForKey:UITransitionContextToViewControllerKey];
+    UIViewController *fromVC = (UIViewController *)[transitionContext viewControllerForKey:UITransitionContextFromViewControllerKey];
+    [inView addSubview:toVC.view];
+    CGRect screenRect = [[UIScreen mainScreen] bounds];
+    [toVC.view setFrame:CGRectMake(0, screenRect.size.height, fromVC.view.frame.size.width, fromVC.view.frame.size.height)];
+    [UIView animateWithDuration:0.25f animations:^{
+        [toVC.view setFrame:CGRectMake(0, 0, fromVC.view.frame.size.width, fromVC.view.frame.size.height)];
+    } completion:^(BOOL finished_) {
+        [transitionContext completeTransition:YES];
+    }];
+}
+
+@end
