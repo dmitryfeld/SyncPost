19a7b4ce87ad220e2b698441a57c0cf11ff48f27
Dmitry Feld <dmitry.feld@icloud.com>
2017-08-01 11:47:35 -0700

    Back end settings are changed

--- Backend/SyncPost/src/java/org/df/syncpost/SyncPost.java
+++ Backend/SyncPost/src/java/org/df/syncpost/SyncPost.java
@@ -11,6 +11,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.df.syncpost.requestmap.DFSPRequestMapProcessor;
 
 /**
  *
@@ -29,21 +30,24 @@ public class SyncPost extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        PrintWriter out = response.getWriter();
-        try {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet SyncPost</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet SyncPost at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        } finally {
-            out.close();
+        
+        if (!DFSPRequestMapProcessor.process(request, response)) {
+            response.setContentType("text/html;charset=UTF-8");
+            PrintWriter out = response.getWriter();
+            try {
+                /* TODO output your page here. You may use following sample code. */
+                out.println("<!DOCTYPE html>");
+                out.println("<html>");
+                out.println("<head>");
+                out.println("<title>Servlet SyncPost</title>");            
+                out.println("</head>");
+                out.println("<body>");
+                out.println("<h1>Servlet SyncPost at " + request.getContextPath() + "</h1>");
+                out.println("</body>");
+                out.println("</html>");
+            } finally {
+                out.close();
+            }
         }
     }
 
--- Backend/SyncPost/web/index.jsp
+++ Backend/SyncPost/web/index.jsp
@@ -12,6 +12,6 @@
         <title>JSP Page</title>
     </head>
     <body>
-        <h1>Hello World!</h1>
+        <h1>Sync Post Service</h1>
     </body>
 </html>
