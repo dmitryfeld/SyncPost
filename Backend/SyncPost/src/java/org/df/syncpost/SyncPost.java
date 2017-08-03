24d8add462202b9afa4ee1f8460a6c1ff6ee7041
Dmitry Feld <dmitry.feld@icloud.com>
2017-08-02 21:41:17 -0700

    Turning to online mode

--- Backend/SyncPost/src/java/org/df/syncpost/SyncPost.java
+++ Backend/SyncPost/src/java/org/df/syncpost/SyncPost.java
@@ -11,7 +11,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import org.df.syncpost.requestmap.DFSPRequestMapProcessor;
+import org.df.syncpost.processors.DFSPRequestMapProcessor;
 
 /**
  *
